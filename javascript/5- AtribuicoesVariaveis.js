e27947f171a5590970e1e2012ce7b28e6c86153a
Hanaju <126880562+Hananaju@users.noreply.github.com>
2023-06-28 09:42:21 -0300

    Delete 5- AtribuicoesVariaveis.js

--- javascript/5- AtribuicoesVariaveis.js
+++
@@ -1,8 +0,0 @@
-console.log("Trabalhando com atribuição de variáveis");
-const idade = 29;
-const nome = "Ricardo";
-const sobrenome = "Bugan";
-
-//console.log(nome + " " + sobrenome);
-console.log(nome, sobrenome);
-console.log('Meu nome é ${nome}');
