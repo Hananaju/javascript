054306e515c7d17d2e1a4cd1986ca1972f3517ef
Hanaju <126880562+Hananaju@users.noreply.github.com>
2023-07-05 08:41:33 -0300

    Update 8- Loops.js

--- javascript/8- Loops.js
+++ javascript/8- Loops.js
@@ -8,7 +8,7 @@ const listaDeDestinos = new Array(
 const idadeComprador = 18;
 const estaAcompanhada = false;
 let temPassagemComprada = true;
-const destino = "São Paulo";
+const destino = "Salvador";
 
 console.log("\n Destinos possíveis:");
 console.log(listaDeDestinos);
@@ -16,11 +16,19 @@ console.log(listaDeDestinos);
 const podeComprar = idadeComprador >= 18 || estaAcompanhada == true;
 
 let contador =0;
+let destinoExiste = false;
 while(contador<3){
     if (listaDeDestinos[contador] == destino){
-        console.log("Destino existe");
-    }else{
-        console.log("Destino não existe");
+        destinoExiste = true;
+        break;
     }
     contador += 1;
+}
+
+console.log("Destino existe: ", destinoExiste)
+
+if(podeComprar && destinoExiste){
+    console.log("Boa VIagem");
+}else{
+    console.log("Desculpe tivemos um erro!");
 }
